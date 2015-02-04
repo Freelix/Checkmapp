15cdeb7eaf8943a6900ac447e98a2c43612d66e7
NamedJohnny <john_forcier2005@hotmail.com>
2015-02-03 19:45:03 -0500

    Ajout de la gestion des POI

--- CheckMapp/ViewModels/POIViewModels/ListPOIViewModel.cs
+++ CheckMapp/ViewModels/POIViewModels/ListPOIViewModel.cs
@@ -4,6 +4,10 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using CheckMapp.Model.DataService;
 using CheckMapp.Model.Tables;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
 
 namespace CheckMapp.ViewModels.POIViewModels
 {
@@ -14,20 +18,26 @@ namespace CheckMapp.ViewModels.POIViewModels
         /// </summary>
         public ListPOIViewModel()
         {
-            /*POI poi1 = new POI();
-            poi1.date = DateTime.Now.AddDays(-2);
-            poi1.nom = "Tour effeil";
-            poi1.ville = "Paris";
+            List<PointOfInterest> poiList = new List<PointOfInterest>();
 
-            POI poi2 = new POI();
-            poi2.date = DateTime.Now;
-            poi2.nom = "Louvre";
-            poi2.ville = "Paris";
+            PointOfInterest poi = new PointOfInterest();
+            poi.City = "Drummondville";
+            poi.Name = "Heriot";
+            poi.Latitude = 20.0;
+            poi.Longitude = 12.0;
 
-            POIList.Add(poi1);
-            POIList.Add(poi2);*/
+            PointOfInterest poi2 = new PointOfInterest();
+            poi2.City = "Victoriaville";
+            poi2.Name = "Heriot";
+            poi2.Latitude = 25.0;
+            poi2.Longitude = 3.0;
 
-            LoadAllPoiFromDatabase();
+            PointOfInterestList = new ObservableCollection<PointOfInterest>();
+
+            PointOfInterestList.Add(poi);
+            PointOfInterestList.Add(poi2);
+
+            //LoadAllPoiFromDatabase();
         }
 
         #region Properties
@@ -48,6 +58,35 @@ namespace CheckMapp.ViewModels.POIViewModels
             get { return "Africa 2014"; }
         }
 
+        private ICommand _deletePOIsCommand;
+        public ICommand DeletePOIsCommand
+        {
+            get
+            {
+                if (_deletePOIsCommand == null)
+                {
+                    _deletePOIsCommand = new RelayCommand<List<PointOfInterest>>((poiList) => DeletePOIs(poiList));
+                }
+                return _deletePOIsCommand;
+            }
+
+        }
+
+        private ICommand _deletePOICommand;
+        public ICommand DeletePOICommand
+        {
+            get
+            {
+                if (_deletePOICommand == null)
+                {
+                    _deletePOICommand = new RelayCommand<PointOfInterest>((poi) => DeletePOI(poi));
+                }
+                return _deletePOICommand;
+            }
+
+        }
+
+       
         #endregion
 
         #region INotifyPropertyChanged Members
@@ -66,6 +105,18 @@ namespace CheckMapp.ViewModels.POIViewModels
         #endregion
 
         #region DBMethods
+
+        public void DeletePOI(PointOfInterest poi)
+        {
+            DataServicePoi dsPoi = new DataServicePoi();
+            
+        }
+
+        public void DeletePOIs(List<PointOfInterest> poiList)
+        {
+            DataServicePoi dsPoi = new DataServicePoi();
+        }
+
 
         public void LoadAllPoiFromDatabase()
         {
